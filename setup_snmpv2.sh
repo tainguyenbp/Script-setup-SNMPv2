be0a5d882c179099a32b900f3c7fc49c7537351d
nntai <nguyenngoctaibp@gmail.com>
2018-10-22 00:30:25 +0700

    Done setup and config snmpv2 on centOS

--- setup_snmpv2.sh
+++ setup_snmpv2.sh
@@ -47,19 +47,38 @@ function config_snmpv2_centos(){
 	hms=$hour$munites$second
 	ymd=$year$month$day
 
-
+	value_systemview=`grep "view    systemview    included   .1" "$PATH_FILE_SNMP" | grep -v "view    systemview    included   .1.3.6.1.2.1.25.1.1" | grep -v "view    systemview    included   .1.3.6.1.2.1.1" | wc -l`
 		if [ -f "$PATH_FILE_SNMP" ]
 			then
 				echo "File $PATH_FILE_SNMP found !!!"
 				# Backup file snmpd.conf 
-				cp "$PATH_FILE_SNMP" /etc/snmp/snmpd.conf_backup_"$ymd"_"$hms"
+				cp "$PATH_FILE_SNMP" "$PATH_FILE_SNMP"_backup_"$ymd"_"$hms"
+				if [ "$value_systemview" == 0 ]
+				then
+					echo "Add string: view    systemview    included   .1"
+					sed -i '/view    systemview    included   .1.3.6.1.2.1.25.1.1/a\view    systemview    included   .1' "$PATH_FILE_SNMP"
 				
+				else
+					echo "String: view    systemview    included   .1"
+				fi
+	value_community=`grep "com2sec notConfigUser  default       " /etc/snmp/snmpd.conf | wc -l`
+		read -p 'Enter string community authentication SNMPv2 : ' string_snmpv2
 
-		
+				if [ "$value_community" = 0 ]
+					then
+					 echo "Add string community authentication SNMPv2 to file $PATH_FILE_SNMP"
+					 sed -i '/com2sec notConfigUser  default       /c \com2sec notConfigUser  default       '$string_snmpv2'' "$PATH_FILE_SNMP"
+				else
+					echo "Add string community authentication SNMPv2 to file $PATH_FILE_SNMP"	
+					sed -i '/com2sec notConfigUser  default       /c \com2sec notConfigUser  default       '$string_snmpv2'' "$PATH_FILE_SNMP"
+				fi
+				
 		else
 			echo "File $PATH_FILE_SNMP not found !!!"
 		fi	
 }
+
+
 
 function setup_snmp_ubuntu(){
 
@@ -78,15 +97,21 @@ function main() {
 					then
 						# setup service snmp on centos 7.x
 						setup_snmp_centos
+						config_snmpv2_centos
+						enable_restart_snmp_centos7
 						
 			elif [ "$OS_DISTRO_VER" \< "$OS_CENTOS_STAND" ];
 					then
                                                 # setup service snmp on centos 5.x to 6.x
                                                 setup_snmp_centos
+						config_snmpv2_centos
+						enable_restart_snmp_centos6
 
 			else
 					 # setup service snmp on centos 7.x
                                                 setup_snmp_centos
+						config_snmpv2_centos
+						enable_restart_snmp_centos7
 			fi
 	
 	elif [[ -f /etc/lsb-release ]];
